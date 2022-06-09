e9d4a4f33304ff44ba7a65275350573725cf0890
Prashant Singh <prashantsing@adobe.com>
2022-06-10 02:55:39 +0530

    Removed extra new line character before class declaration

--- src/test/java/primitives_types/linked_lists/LinkedListReverseSublistTest.java
+++ src/test/java/primitives_types/linked_lists/LinkedListReverseSublistTest.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 class LinkedListReverseSublistTest {
 
     public static <T> List<T> list(T... values) {
