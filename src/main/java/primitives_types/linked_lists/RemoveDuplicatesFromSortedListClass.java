a98e83ca5cad2334fd8b16cabde522cdc8587c4a
Prashant Singh <prashantsing@adobe.com>
2022-06-13 00:29:38 +0530

    Revert "Revert "Remove duplicates from sorted linked list by k nodes""
    This reverts commit 8941b08dd4a83c7eba41098cd00f5df878c94528.

---
+++ src/main/java/primitives_types/linked_lists/RemoveDuplicatesFromSortedListClass.java
@@ -0,0 +1,35 @@
+package primitives_types.linked_lists;
+
+import primitives_types.linked_lists.cores.SinglyLinkedListNode;
+
+import java.util.Objects;
+
+public class RemoveDuplicatesFromSortedListClass<T extends Comparable<T>> {
+
+    private final SinglyLinkedListNode<T> firstNode;
+
+    public RemoveDuplicatesFromSortedListClass(SinglyLinkedListNode<T> firstNode) {
+        if (Objects.isNull(firstNode))
+            throw new NullInput();
+        this.firstNode = firstNode;
+
+    }
+
+    public void removeDuplicates() {
+        SinglyLinkedListNode<T> iterator = firstNode;
+        while (Objects.nonNull(iterator) && Objects.nonNull(iterator.getNext())) {
+            if (iterator.getData().equals(iterator.getNext().getData())) {
+                iterator.setNext(iterator.getNext().getNext());
+            } else {
+                iterator = iterator.getNext();
+            }
+        }
+    }
+
+    public SinglyLinkedListNode<T> getFirstNode() {
+        return firstNode;
+    }
+
+    public static class NullInput extends RuntimeException {
+    }
+}
