b61204658111e45b0dabe940cee0eaceb03a703f
Prashant Singh <prashantsing@adobe.com>
2022-06-17 17:31:06 +0530

    Print reverse order code and tests

---
+++ src/main/java/primitives_types/linked_lists/PrintReverseOrder.java
@@ -0,0 +1,28 @@
+package primitives_types.linked_lists;
+
+import primitives_types.linked_lists.cores.SinglyLinkedListNode;
+import primitives_types.strings.StringUtils;
+
+import java.util.Objects;
+
+public class PrintReverseOrder<T> {
+    public static final String SEPARATOR = " ";
+    private SinglyLinkedListNode<T> head;
+    private String result;
+
+    public PrintReverseOrder(SinglyLinkedListNode<T> head) {
+        this.head = head;
+        this.result = "";
+    }
+
+    public String getReverseStringData() {
+        result = "";
+        printReverseHelper(head);
+        return result.trim();
+    }
+
+    private void printReverseHelper(SinglyLinkedListNode<T> head) {
+        if (Objects.nonNull(head) && Objects.nonNull(head.getNext())) printReverseHelper(head.getNext());
+        result = result.concat(Objects.nonNull(head) ? head.getData().toString() : StringUtils.EMPTY).concat(SEPARATOR);
+    }
+}
